5ee5aa3607d6ef95cf8eb5e4ed1c035c1161ce38
Paul Sanders <psanders1@gmail.com>
2022-11-21 20:26:29 -0500

    Add option to use Client in task functions

--- meilisearch_python_async/task.py
+++ meilisearch_python_async/task.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from asyncio import sleep
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from httpx import AsyncClient
 
@@ -9,13 +10,16 @@ from meilisearch_python_async._http_requests import HttpRequests
 from meilisearch_python_async.errors import MeiliSearchTimeoutError
 from meilisearch_python_async.models.task import TaskStatus
 
+if TYPE_CHECKING:
+    from meilisearch_python_async.client import Client  # pragma: no cover
 
-async def get_tasks(http_client: AsyncClient, index_id: str | None = None) -> list[TaskStatus]:
+
+async def get_tasks(client: AsyncClient | Client, index_id: str | None = None) -> list[TaskStatus]:
     """Get all tasks.
 
     Args:
 
-        http_client: An AsyncClient instance.
+        client: An httpx AsyncClient or meilisearch_python_async Client instance.
         index_id: The id of the index for which to get the tasks. If provided this will get the
             tasks only for the specified index, if not all tasks will be returned. Default = None
 
@@ -35,21 +39,22 @@ async def get_tasks(http_client: AsyncClient, index_id: str | None = None) -> li
         >>> from meilisearch_python_async.task import get_tasks
         >>>
         >>> async with Client("http://localhost.com", "masterKey") as client:
-        >>>     await get_tasks(client.http_client)
+        >>>     await get_tasks(client)
     """
     url = f"tasks?indexUid={index_id}" if index_id else "tasks"
-    response = await http_client.get(url)
+    client_ = _get_client(client)
+    response = await client_.get(url)
     tasks = [TaskStatus(**x) for x in response.json()["results"]]
 
     return tasks
 
 
-async def get_task(http_client: AsyncClient, task_id: int) -> TaskStatus:
+async def get_task(client: AsyncClient | Client, task_id: int) -> TaskStatus:
     """Get a single task from it's task id.
 
     Args:
 
-        http_client: An AsyncClient instance.
+        client: An httpx AsyncClient or meilisearch_python_async Client instance.
         task_id: Identifier of the task to retrieve.
 
     Returns:
@@ -68,21 +73,26 @@ async def get_task(http_client: AsyncClient, task_id: int) -> TaskStatus:
         >>> from meilisearch_python_async.task import get_task
         >>>
         >>> async with Client("http://localhost.com", "masterKey") as client:
-        >>>     await get_task(client.http_client, 1244)
+        >>>     await get_task(client, 1244)
     """
-    response = await http_client.get(f"tasks/{task_id}")
+    client_ = _get_client(client)
+    response = await client_.get(f"tasks/{task_id}")
 
     return TaskStatus(**response.json())
 
 
 async def wait_for_task(
-    http_client: AsyncClient, task_id: int, *, timeout_in_ms: int = 5000, interval_in_ms: int = 50
+    client: AsyncClient | Client,
+    task_id: int,
+    *,
+    timeout_in_ms: int = 5000,
+    interval_in_ms: int = 50,
 ) -> TaskStatus:
     """Wait until MeiliSearch processes a task, and get its status.
 
     Args:
 
-        http_client: An AsyncClient instance.
+        client: An httpx AsyncClient or meilisearch_python_async Client instance.
         task_id: Identifier of the task to retrieve.
         timeout_in_ms: Amount of time in milliseconds to wait before raising a
             MeiliSearchTimeoutError. Defaults to 5000.
@@ -109,10 +119,11 @@ async def wait_for_task(
         >>> async with Client("http://localhost.com", "masterKey") as client:
         >>>     index = client.index("movies")
         >>>     response = await index.add_documents(documents)
-        >>>     await wait_for_pending_task(client.http_client, response.update_id)
+        >>>     await wait_for_pending_task(client, response.update_id)
     """
+    client_ = _get_client(client)
     url = f"tasks/{task_id}"
-    http_requests = HttpRequests(http_client)
+    http_requests = HttpRequests(client_)
     start_time = datetime.now()
     elapsed_time = 0.0
     while elapsed_time < timeout_in_ms:
@@ -126,3 +137,10 @@ async def wait_for_task(
     raise MeiliSearchTimeoutError(
         f"timeout of {timeout_in_ms}ms has exceeded on process {task_id} when waiting for pending update to resolve."
     )
+
+
+def _get_client(client: AsyncClient | Client) -> AsyncClient:
+    if isinstance(client, AsyncClient):
+        return client
+
+    return client.http_client
