4322a9d09f6e190daa64151b8a96f6bb24898c1a
Paul Sanders <psanders1@gmail.com>
2023-05-10 10:37:25 -0400

    Add user agent

---
+++ tests/test_version.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from meilisearch_python_async import __version__
+from meilisearch_python_async._http_requests import user_agent
+from meilisearch_python_async._version import get_version
+
+try:
+    import tomli as tomllib  # type: ignore
+except ModuleNotFoundError:
+    import tomllib  # type: ignore
+
+
+def test_versions_match():
+    pyproject = Path().absolute() / "pyproject.toml"
+    with open(pyproject, "rb") as f:
+        data = tomllib.load(f)
+        pyproject_version = data["tool"]["poetry"]["version"]
+
+    assert get_version() == pyproject_version
+
+
+def test_user_agent():
+    assert user_agent() == f"Meilisearch Python Async (v{__version__})"
