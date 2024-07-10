a591fe71af1096b83608ba6ab0c255eff693b8fb
Paul Sanders <paul@paulsanders.dev>
2024-07-09 22:57:11 -0400

    Add typing-extension (#987)
    * Add typing-extension
    
    * Add no cover

--- meilisearch_python_sdk/types.py
+++ meilisearch_python_sdk/types.py
@@ -1,5 +1,11 @@
+import sys
 from typing import Any, Dict, List, MutableMapping, Union
 
-Filter = Union[str, List[Union[str, List[str]]]]
-JsonDict = Dict[str, Any]
-JsonMapping = MutableMapping[str, Any]
+if sys.version_info >= (3, 10):  # pragma: no cover
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+Filter: TypeAlias = Union[str, List[Union[str, List[str]]]]
+JsonDict: TypeAlias = Dict[str, Any]
+JsonMapping: TypeAlias = MutableMapping[str, Any]
