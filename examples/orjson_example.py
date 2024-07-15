b8cbb47c58cdd90d9529375993e543451e20a4d0
Paul Sanders <paul@paulsanders.dev>
2024-07-15 10:08:40 -0400

    Add orjson and ujson examples

---
+++ examples/orjson_example.py
@@ -0,0 +1,14 @@
+from meilisearch_python_sdk import Client
+from meilisearch_python_sdk.json_handler import OrjsonHandler
+
+
+def main() -> int:
+    client = Client("http://127.0.0.1:7700", "masterKey", json_handler=OrjsonHandler())
+    index = client.create_index("movies", primary_key="id")
+    index.add_documents_from_file("../datasets/small_movies.json")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
