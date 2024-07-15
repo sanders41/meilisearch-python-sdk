3c6951e74d4ad11c7398a1a7beed08c4acbf147a
Paul Sanders <paul@paulsanders.dev>
2024-07-15 09:34:23 -0400

    Add fastapi example

--- examples/README.md
+++ examples/README.md
@@ -13,3 +13,13 @@ the example files, i.e.:
 ```sh
 python add_documents_decorator.py
 ```
+
+## FastAPI Example
+
+To run the FastAPI example run
+
+```sh
+fastapi dev fastapi_example.py
+```
+
+Then go to [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs) to test the routes.
---
+++ examples/fastapi_example.py
@@ -0,0 +1,82 @@
+from contextlib import asynccontextmanager
+from typing import Any, AsyncGenerator, Dict, List
+
+from fastapi import Depends, FastAPI, HTTPException
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+from typing_extensions import Annotated
+
+from meilisearch_python_sdk import AsyncClient, AsyncIndex
+from meilisearch_python_sdk.errors import MeilisearchApiError, MeilisearchCommunicationError
+from meilisearch_python_sdk.models.documents import DocumentsInfo
+from meilisearch_python_sdk.models.health import Health
+from meilisearch_python_sdk.models.search import SearchParams, SearchResults
+from meilisearch_python_sdk.models.task import TaskInfo
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    try:
+        health = await client.health()
+        if health.status != "available":
+            raise HTTPException(
+                status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="The Meilisearch server is not available",
+            )
+    except MeilisearchCommunicationError as e:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unable to connect to the Meilisearch server",
+        ) from e
+    yield
+    await client.aclose()  # Shutdown the client when exiting the app
+
+
+app = FastAPI(lifespan=lifespan)
+client = AsyncClient("http://127.0.0.1:7700", "masterKey")
+
+
+async def get_index() -> AsyncIndex:
+    try:
+        index = await client.get_index("movies")
+    except MeilisearchApiError as e:
+        if e.status_code == 404:  # If the index movies does not already exist create it
+            index = await client.create_index("movies", primary_key="id")
+        else:
+            raise
+    return index
+
+
+@app.get("/health")
+async def check_health() -> Health:
+    return await client.health()
+
+
+@app.get("/documents")
+async def get_documents(index: Annotated[AsyncIndex, Depends(get_index)]) -> DocumentsInfo:
+    return await index.get_documents()
+
+
+@app.post("/documents")
+async def add_documents(
+    documents: List[Dict[str, Any]], index: Annotated[AsyncIndex, Depends(get_index)]
+) -> List[TaskInfo]:
+    return await index.add_documents_in_batches(documents)
+
+
+@app.post("/search")
+async def search(
+    search_params: SearchParams, index: Annotated[AsyncIndex, Depends(get_index)]
+) -> SearchResults:
+    params = search_params.model_dump()
+    del params["index_uid"]
+    return await index.search(**params)
+
+
+def main() -> int:
+    app()
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
