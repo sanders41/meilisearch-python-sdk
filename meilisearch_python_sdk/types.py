0833a4db9faa0daaa113920f3d17df656e0198f7
Paul Sanders <paul@paulsanders.dev>
2024-07-16 09:51:30 -0400

    Use unions for python 3.9

--- meilisearch_python_sdk/types.py
+++ meilisearch_python_sdk/types.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 import sys
 from collections.abc import MutableMapping
-from typing import Any
+from typing import Any, Union
 
 if sys.version_info >= (3, 10):  # pragma: no cover
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias
 
-Filter: TypeAlias = str | list[str | list[str]]
+Filter: TypeAlias = Union[str, list[Union[str, list[str]]]]
 JsonDict: TypeAlias = dict[str, Any]
 JsonMapping: TypeAlias = MutableMapping[str, Any]
