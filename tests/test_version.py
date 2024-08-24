d6e719b0252af9c68592573e508e22695b3ecbcc
Paul Sanders <paul@paulsanders.dev>
2024-08-24 15:12:59 -0400

    Switch to uv (#1058)
    * Switch to uv
    
    * Fix variable
    
    * Update CONTRIBUTING file
    
    * Install uv with curl in ci

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,23 +1,5 @@
-import sys
-from pathlib import Path
-
 from meilisearch_python_sdk import __version__
 from meilisearch_python_sdk._http_requests import user_agent
-from meilisearch_python_sdk._version import VERSION
-
-if sys.version_info < (3, 11):
-    import tomli as tomllib
-else:
-    import tomllib
-
-
-def test_versions_match():
-    pyproject = Path().absolute() / "pyproject.toml"
-    with open(pyproject, "rb") as f:
-        data = tomllib.load(f)
-        pyproject_version = data["tool"]["poetry"]["version"]
-
-    assert VERSION == pyproject_version
 
 
 def test_user_agent():
