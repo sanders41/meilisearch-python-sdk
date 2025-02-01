ab445aaf05937e66f068710c2689e37c486221a6
Paul Sanders <paul@paulsanders.dev>
2025-02-01 18:49:25 -0500

    Add no cover to typechecking

--- meilisearch_python_sdk/types.py
+++ meilisearch_python_sdk/types.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from collections.abc import MutableMapping
 from typing import TYPE_CHECKING, Any, Union
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     import sys
 
-    if sys.version_info >= (3, 10):  # pragma: no cover
+    if sys.version_info >= (3, 10):
         from typing import TypeAlias
     else:
         from typing_extensions import TypeAlias
