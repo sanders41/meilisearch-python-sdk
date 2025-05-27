bd620ab8cc7c0ad54c8db28270036f0a792cf71f
Paul Sanders <paul@paulsanders.dev>
2025-05-27 08:32:47 -0400

    Bump pytest-asyncio 0.26.0 -> 1.0.0
    Also add event loop scopes to pyproject.toml

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from uuid import uuid4
 
 import pytest
-from pytest_asyncio import is_async_test
 
 try:
     import truststore as truststore
@@ -35,13 +34,6 @@ MASTER_KEY = "masterKey"
 
 ROOT_PATH = Path().absolute()
 SMALL_MOVIES_PATH = ROOT_PATH / "datasets" / "small_movies.json"
-
-
-def pytest_collection_modifyitems(items):
-    pytest_asyncio_tests = (item for item in items if is_async_test(item))
-    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
-    for async_test in pytest_asyncio_tests:
-        async_test.add_marker(session_scope_marker, append=False)
 
 
 def pytest_addoption(parser):
