d5cce1acd5da83cf2e616a8d9b1f93b7e76ee92e
Paul Sanders <paul@paulsanders.dev>
2025-12-23 12:12:24 -0500

    Fix tests

--- tests/test_http_requests.py
+++ tests/test_http_requests.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 async def test_async_empty_bytes_compressed(async_client):
     index = async_client.index(str(uuid4()))
     response = await index._http_requests.post(
-        index._documents_url, body=b"''", content_type="text/csv"
+        index._documents_url, body=b"''", content_type="text/csv", compress=True
     )
 
     assert response.status_code == 202
@@ -14,7 +14,7 @@ async def test_async_empty_byte_array_compressed(async_client):
     index = async_client.index(str(uuid4()))
     b = bytearray(b"''")
     response = await index._http_requests.post(
-        index._documents_url, body=b, content_type="text/csv"
+        index._documents_url, body=b, content_type="text/csv", compress=True
     )
 
     assert response.status_code == 202
@@ -22,7 +22,9 @@ async def test_async_empty_byte_array_compressed(async_client):
 
 def test_empty_bytes_compressed(client):
     index = client.index(str(uuid4()))
-    response = index._http_requests.post(index._documents_url, body=b"''", content_type="text/csv")
+    response = index._http_requests.post(
+        index._documents_url, body=b"''", content_type="text/csv", compress=True
+    )
 
     assert response.status_code == 202
 
@@ -30,6 +32,8 @@ def test_empty_bytes_compressed(client):
 def test_empty_byte_array_compressed(client):
     index = client.index(str(uuid4()))
     b = bytearray(b"''")
-    response = index._http_requests.post(index._documents_url, body=b, content_type="text/csv")
+    response = index._http_requests.post(
+        index._documents_url, body=b, content_type="text/csv", compress=True
+    )
 
     assert response.status_code == 202
