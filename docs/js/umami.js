8b3cd6a980b17d56291d95320bcc11a39e0f17db
Paul Sanders <paul@paulsanders.dev>
2026-01-17 21:38:31 -0500

    Update website ID in umami.js script

--- docs/js/umami.js
+++ docs/js/umami.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var umamiScript = document.createElement("script");
   umamiScript.defer = true;
   umamiScript.src = "https://cloud.umami.is/script.js";
-  umamiScript.dataset.websiteId = "29a9d193-92a3-44d0-9b1d-16ddd23898fb";
+  umamiScript.dataset.websiteId = "c1557f38-bc02-4277-82b4-22a7029f42ba";
   document.head.appendChild(umamiScript);
 });
