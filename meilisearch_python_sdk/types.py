969d2e8312c408d76d59a453e3500b76364e7044
Paul Sanders <paul@paulsanders.dev>
2026-02-28 21:05:48 -0500

    Refactor plugin filtering

--- meilisearch_python_sdk/types.py
+++ meilisearch_python_sdk/types.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from collections.abc import MutableMapping
-from typing import Any, TypeAlias
+from typing import Any, Literal, TypeAlias
 
 Filter: TypeAlias = str | list[str | list[str]]
 JsonDict: TypeAlias = dict[str, Any]
 JsonMapping: TypeAlias = MutableMapping[str, Any]
+PluginEvent: TypeAlias = Literal["CONCURRENT_EVENT", "POST_EVENT", "PRE_EVENT"]
